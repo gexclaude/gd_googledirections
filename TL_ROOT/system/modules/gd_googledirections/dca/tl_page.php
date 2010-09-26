e5341a1125f0d925391b4adb102d2e366935b634
Claude Gex <mail@claudegex.ch>
2010-09-26 12:08:17 +0200

    readd sources

---
+++ TL_ROOT/system/modules/gd_googledirections/dca/tl_page.php
@@ -0,0 +1,44 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight webCMS
+ * Copyright (C) 2005-2009 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at http://www.gnu.org/licenses/.
+ *
+ * PHP version 5
+ * @copyright  Claude Gex 2010
+ * @author     Claude Gex <mail@claudegex.ch>
+ * @license    LGPL
+ */
+
+/**
+ * Modify the root palette for tl_page
+ */
+
+$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] .= ';{gd_googlemaps_legend},gd_googlemaps_id';
+
+/**
+ * Add fields to tl_page
+ */
+$GLOBALS['TL_DCA']['tl_page']['fields']['gd_googlemaps_id'] =  array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['gd_googlemaps_id'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('mandatory'=>false, 'maxlength'=>255)
+);
+
+?>
